39047b4e53e45260839d43f8cc26f78726dc0ee0
ziguiway <ziguiway@163.com>
2022-08-08 13:59:59 +0800

    第一次提交

---
+++ src/main/java/com/ziguiway/adventurechat/AdventureChatApplication.java
@@ -0,0 +1,12 @@
+package com.ziguiway.adventurechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureChatApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdventureChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ziguiway/adventurechat/AdventureChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ziguiway.adventurechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventureChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
